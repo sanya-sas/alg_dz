b4551ba0357aca33ac310f876af059f39c1eee6e
llinaaa1 <lashenova.polina@gmail.com>
2025-12-14 18:46:38 +0300

    Create lashenova.cpp

---
+++ lashenova.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+int coinChange(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, INT_MAX - 1);
+    dp[0] = 0;
+
+    for (int i = 1; i <= amount; i++) {
+        for (int coin : coins) {
+            if (coin <= i) {
+                dp[i] = min(dp[i], dp[i - coin] + 1);
+            }
+        }
+    }
+
+    return dp[amount] == INT_MAX - 1 ? -1 : dp[amount];
+}
+
+int coinChangeWays(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, 0);
+    dp[0] = 1;
+
+    for (int coin : coins) {
+        for (int i = coin; i <= amount; i++) {
+            dp[i] += dp[i - coin];
+        }
+    }
+
+    return dp[amount];
+}
+
+int main() {
+    return 0;
+}
